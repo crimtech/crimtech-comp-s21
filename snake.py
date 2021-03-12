7a305fb66ed12a412257bcad82b1b2f2e9ff8c19
alexa2001 <amlagunas70@gmail.com>
2021-03-11 19:42:43 -0500

    Submit Project 2

--- snake.py
+++ snake.py
@@ -1,3 +1,4 @@
+
 import random
 import pygame
 import sys
@@ -19,9 +20,11 @@ DIR = {
 
 class Snake(object):
     l = 1
-    body = [(WIDTH // 2 + 1, HEIGHT // 2),(WIDTH // 2, HEIGHT // 2)]
+    body = [ (WIDTH // 2 + 1, HEIGHT // 2) , (WIDTH // 2, HEIGHT // 2) ]
     direction = 'r'
+    prev = ''
     dead = False
+    length=2
 
     def __init__(self):
         pass
@@ -36,7 +39,14 @@ class Snake(object):
 
     def turn(self, dir):
         # TODO: See section 3, "Turning the snake".
+        self.prev = self.direction
         self.direction = dir
+
+        # if (self.direction=='r'):
+        #     temp=self.get_head()
+            # self.body[]
+
+
 
     def collision(self, x, y):
         # TODO: See section 2, "Collisions", and section 4, "Self Collisions"
@@ -54,14 +64,38 @@ class Snake(object):
         # TODO: See section 13, "coyote time".
         pass
 
-    def move(self):
-        # TODO: See section 1, "Move the snake!". You will be revisiting this section a few times.
-    
-        self.body=[(31,30), (30,30)]
+    def move(self, apple):
+        # TODO: See section 1, "Move the snake!". You will be revisiting this section a few times. 
         head=self.body[0]
+        tail=self.body[-1]
+        change=DIR[self.direction]
+        coord = self.body[0] 
+        new_coord = (coord[0] + change[0], coord[1] + change[1])
+
+        if (self.prev=="r" and self.direction=="l"):
+            self.kill()
+        if (self.prev=="l" and self.direction=="r"):
+            self.kill()
+        if (self.prev=="u" and self.direction=="d"):
+            self.kill()
+        if (self.prev=="d" and self.direction=="u"):
+            self.kill()
+
+
+
+        self.body.insert(0, new_coord)
+        self.body.remove(tail)
+
+        if (self.body[0]==apple):
+            self.body.append(tail)
+
+        for i in range(1, len(self.body)):
+            if (self.body[0] == self.body[i]):
+                self.kill()
 
         if self.collision(head[0], head[1]):
             self.kill()
+    
 
     def kill(self):
         # TODO: See section 11, "Try again!"
@@ -72,7 +106,9 @@ class Snake(object):
             p = self.body[i]
             pos = (p[0] * SIZE, p[1] * SIZE)
             r = pygame.Rect(pos, (SIZE, SIZE))
-            pygame.draw.rect(surface, self.get_color(i), r)
+            # pygame.draw.rect(surface, self.get_color(i), r)
+            pygame.draw.rect(surface, (0, 0, 0), r)
+
 
     def handle_keypress(self, k):
         if k == pygame.K_UP:
@@ -110,7 +146,16 @@ class Apple(object):
 
     def place(self, snake):
         # TODO: see section 6, "moving the apple".
-        pass
+        randx = random.randint(0,23)
+        randy = random.randint(0,23)
+
+        for i in range(len(snake)):
+            if i==(randx, randy):
+                randx = random.randint(0,24)
+                randy = random.randint(0,24)
+
+        self.position=(randx, randy)
+      
 
     def draw(self, surface):
         pos = (self.position[0] * SIZE, self.position[1] * SIZE)
@@ -126,6 +171,16 @@ def draw_grid(surface):
 
 def main():
     pygame.init()
+    pygame.font.init()
+
+    # display_surface = pygame.display.set_mode((12, 12))
+    # pygame.display.set_caption('Show Text')
+    # font = pygame.font.Font('freesansbold.ttf', 32)
+    # green = (0, 255, 0)
+    # blue = (0, 0, 128)
+    # text = font.render('GeeksForGeeks', True, green, blue)
+    # textRect = text.get_rect()
+    
 
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
@@ -139,19 +194,33 @@ def main():
 
     score = 0
 
+    # myfont = pygame.font.SysFont('Comic Sans MS', 30)
+    # textsurface = myfont.render(str(score), True, (0, 0, 0))
+
     while True:
         # TODO: see section 10, "incremental difficulty".
-        clock.tick(10)
+        # display_surface.blit(text, textRect)
+        
+        clock.tick(8)
         snake.check_events()
         draw_grid(surface)        
-        snake.move()
+        snake.move(apple.position)
+        # print(snake.get_head())
 
         snake.draw(surface)
         apple.draw(surface)
+        
         # TODO: see section 5, "Eating the Apple".
+        if snake.get_head() == apple.position:
+            print("the snake at an apple!")
+            apple.place(snake.body) #i think this is the issue
+            score=score+1
+        
         screen.blit(surface, (0,0))
         # TODO: see section 8, "Display the Score"
-
+        myfont = pygame.font.SysFont('Comic Sans MS', 30)
+        textsurface = myfont.render(str(score), True, (0, 0, 0))
+        screen.blit(textsurface,(10,10))
         pygame.display.update()
         if snake.dead:
             print('You died. Score: %d' % score)
